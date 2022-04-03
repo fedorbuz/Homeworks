534163762790b916ba7c442cb66e59d99a594fe6
fedor <fedor.buz@gmail.com>
2022-04-03 13:53:43 +0300

    base.Vehicle updated

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,18 @@
 from abc import ABC
-
+import exceptions
 
 class Vehicle(ABC):
-    pass
+    def __init__(self, weight=1000, started=True, fuel=75, fuel_consumption=14):
+        self.weight = weight
+        self.started = started
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start (self):
+        if self.fuel > 0:
+            self.started = True
+        else:
+            raise exceptions.LowFuelError
+
+
+
