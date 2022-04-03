405432676e37be19fde11c3f1f4181958d6b6ff3
fedor <fedor.buz@gmail.com>
2022-04-03 15:29:41 +0300

    updated

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,14 +1,20 @@
 from abc import ABC
 import exceptions
 
+
 class Vehicle(ABC):
-    def __init__(self, weight=1000, started=True, fuel=75, fuel_consumption=14): #расход в л/100 км
+
+    weight = 1000
+    started = True
+    fuel = 75
+    fuel_consumption = 14  # расход в л/100 км
+
+    def __init__(self, weight, fuel, fuel_consumption):
         self.weight = weight
-        self.started = started
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
 
-    def start (self):
+    def start(self):
         if self.fuel > 0:
             self.started = True
         else:
@@ -17,7 +23,5 @@ class Vehicle(ABC):
     def move(self, distance):
         if self.fuel / (self.fuel_consumption / 100) > distance:
             self.fuel -= self.fuel_consumption * distance / 100
-
-
-
-
+        else:
+            raise exceptions.NotEnoughFuel
--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,13 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from homework_02 import engine
+from homework_02.base import Vehicle
+
+
+class Car(Vehicle):
+    engine = None
+
+
+
+
