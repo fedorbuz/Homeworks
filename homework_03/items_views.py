6c78e1a7a1914b9095e7e9599bca004a48bc36b9
fedor <fedor.buz@gmail.com>
2022-04-18 22:44:23 +0300

    router upd users_views add

--- homework_03/items_views.py
+++ homework_03/items_views.py
@@ -5,3 +5,11 @@ router = APIRouter(tags=["Items"], prefix="/items")
 @router.get("")
 def get_items():
     return [{"item_id": 1}]
+
+@router.get("/{item_id}")
+def get_item(item_id: int):
+    return {"item_id": item_id}
+
+@router.post("")
+def create_item(data: dict):
+    return {"data": data}
